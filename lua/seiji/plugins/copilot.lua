ecb7b4faca5b5638e64f4c3a02f5e2cef27bc126
seiji <51401797+seijidev@users.noreply.github.com>
2025-01-16 20:41:59 -0900

    Add copilot

--- lua/seiji/plugins/copilot.lua
+++ lua/seiji/plugins/copilot.lua
@@ -1,2 +1,20 @@
 return {
-  zbirenbaum/copilot.lua
+  'zbirenbaum/copilot.lua',
+  cmd = 'Copilot',
+  --build = ':Copilot auth',
+  event = 'InsertEnter',
+  opts = {
+    suggestion = {
+      enabled = true,
+      auto_trigger = true,
+      keymap = {
+        accept = '<C-CR>',
+      },
+    },
+    panel = { enabled = false },
+    filetypes = {
+      markdown = true,
+      help = true,
+    },
+  },
+}
