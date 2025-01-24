aaa75c925c068653ca10b351087dace00037577d
seiji <51401797+seijidev@users.noreply.github.com>
2025-01-23 19:07:26 -0900

    Make blink.cmp scroll document configurable

--- lua/seiji/plugins/blink-cmp.lua
+++ lua/seiji/plugins/blink-cmp.lua
@@ -18,7 +18,19 @@ return {
       -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
       -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
       -- See the full "keymap" documentation for information on defining your own keymap.
-      keymap = { preset = 'default' },
+      keymap = {
+        preset = 'default',
+        ['<C-f>'] = {
+          function(cmp)
+            cmp.scroll_documentation_up(4)
+          end,
+        },
+        ['<C-b>'] = {
+          function(cmp)
+            cmp.scroll_documentation_down(4)
+          end,
+        },
+      },
 
       appearance = {
         -- Sets the fallback highlight groups to nvim-cmp's highlight groups
