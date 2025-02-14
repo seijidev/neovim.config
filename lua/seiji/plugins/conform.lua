c1dfe863ee21ebfe796893efb6281bf2226f8e9f
seiji <51401797+seijidev@users.noreply.github.com>
2025-02-14 09:43:54 -0900

    Enable formatter for Python and formatter keymap

--- lua/seiji/plugins/conform.lua
+++ lua/seiji/plugins/conform.lua
@@ -3,18 +3,16 @@ return {
     'stevearc/conform.nvim',
     event = { 'BufWritePre' },
     cmd = { 'ConformInfo' },
-    --[[
     keys = {
       {
-        '<leader>f',
+        '<leader>fm',
         function()
           require('conform').format { async = true, lsp_format = 'fallback' }
         end,
         mode = '',
-        desc = '[F]ormat buffer',
+        desc = '[F]or[m]at buffer',
       },
     },
-    --]]
     opts = {
       notify_on_error = false,
       format_on_save = function(bufnr)
@@ -36,7 +34,7 @@ return {
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
+        python = { 'isort', 'black' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
