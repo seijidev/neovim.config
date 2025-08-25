6cc95a27a9ee7db86681d8b68e3e664f1734f982
seiji <51401797+seijidev@users.noreply.github.com>
2025-08-25 15:47:46 -0800

    Refactor colorscheme config

--- lua/seiji/plugins/colorscheme.lua
+++ lua/seiji/plugins/colorscheme.lua
@@ -1,69 +1,73 @@
---[[
 return { -- You can easily change to a different colorscheme.
   -- Change the name of the colorscheme plugin below, and then
   -- change the command in the config to whatever the name of that colorscheme is.
   --
-  'gambhirsharma/vesper.nvim',
-  lazy = false,
-  priority = 1000, -- Make sure to load this before all the other start plugins.
-  init = function()
-    -- Load the colorscheme here.
-    -- Like many other themes, this one has different styles, and you could load
-    -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-    vim.cmd.colorscheme 'vesper'
+  {
+    'gambhirsharma/vesper.nvim',
+    enabled = false,
+    lazy = false,
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    init = function()
+      -- Load the colorscheme here.
+      -- Like many other themes, this one has different styles, and you could load
+      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+      vim.cmd.colorscheme 'vesper'
 
-    -- You can configure highlights by doing something like:
-    vim.cmd.hi 'Comment gui=none'
-  end,
-},
---]]
--- return {
---   'rebelot/kanagawa.nvim',
---   lazy = false,
---   priority = 1000,
---   opts = {},
---   init = function()
---     vim.cmd.colorscheme 'kanagawa-wave'
---     vim.cmd.hi 'Comment gui=none'
---   end,
--- }
+      -- You can configure highlights by doing something like:
+      vim.cmd.hi 'Comment gui=none'
+    end,
+  },
 
--- return {
---   'sho-87/kanagawa-paper',
---   lazy = false,
---   priority = 1000,
---   opts = {},
---   init = function()
---     vim.cmd.colorscheme 'kanagawa-paper'
---     vim.cmd.hi 'Comment gui=none'
---   end,
--- }
---
--- return {
---   'olivercederborg/poimandres.nvim',
---   lazy = false,
---   priority = 1000,
---   config = function()
---     require('poimandres').setup {
---       -- leave this setup function empty for default config
---       -- or refer to the configuration section
---       -- for configuration options
---     }
---   end,
---
---   -- optionally set the colorscheme within lazy config
---   init = function()
---     vim.cmd 'colorscheme poimandres'
---   end,
--- }
+  {
+    'rebelot/kanagawa.nvim',
+    enabled = false,
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    init = function()
+      vim.cmd.colorscheme 'kanagawa-wave'
+      vim.cmd.hi 'Comment gui=none'
+    end,
+  },
 
-return {
-  'eldritch-theme/eldritch.nvim',
-  lazy = false,
-  priority = 1000,
-  opts = {},
-  init = function()
-    vim.cmd.colorscheme 'eldritch'
-    vim.cmd.hi 'Comment gui=none'
-  end,
+  {
+    'sho-87/kanagawa-paper',
+    enabled = false,
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    init = function()
+      vim.cmd.colorscheme 'kanagawa-paper'
+      vim.cmd.hi 'Comment gui=none'
+    end,
+  },
+
+  {
+    'olivercederborg/poimandres.nvim',
+    enabled = false,
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('poimandres').setup {
+        -- leave this setup function empty for default config
+        -- or refer to the configuration section
+        -- for configuration options
+      }
+    end,
+    -- optionally set the colorscheme within lazy config
+    init = function()
+      vim.cmd 'colorscheme poimandres'
+    end,
+  },
+  {
+    'eldritch-theme/eldritch.nvim',
+    enabled = true,
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    init = function()
+      vim.cmd.colorscheme 'eldritch'
+      vim.cmd.hi 'Comment gui=none'
+    end,
+  },
 }
